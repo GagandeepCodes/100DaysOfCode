d17ffd26c58c1d39c8b1d2ecf3006bb82731bb52
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2022-09-18 00:08:09 +0530

    Day 6 - 18 Sept 2022:1

--- Day6/Rotate_an_Array.cpp
+++ Day6/Rotate_an_Array.cpp
@@ -1,13 +1,32 @@
-void rotateArr(int arr[], int d, int n){
-        // code here
-        int temp[n];
-        for(int i=0;i<n;i++){
-            if((i-d)<0){
-                index = n-i;
-            }
-            else
-            {
-                
-            }
+#include<bits/stdc++.h>
+using namespace std;
+void rotateArr(int arr[], int d, int n)
+{
+    // code here
+    int temp[n];
+    d = d % n;
+    for (int i = 0; i < n; i++)
+    {
+        int index = -1;
+        if ((i - d) < 0)
+        {
+            index = n - d + i;
         }
+        else
+        {
+            index = i - d;
+        }
+
+        temp[index] = arr[i];
     }
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = temp[i];
+    }
+}
+int main()
+{
+    
+    return 0;
+}
