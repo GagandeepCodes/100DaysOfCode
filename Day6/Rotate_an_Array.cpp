a8631c8ea357050132da04b70929d993b0e535b4
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2022-09-17 23:47:32 +0530

    Day 6 - 17 Sept 2022:1

---
+++ Day6/Rotate_an_Array.cpp
@@ -0,0 +1,13 @@
+void rotateArr(int arr[], int d, int n){
+        // code here
+        int temp[n];
+        for(int i=0;i<n;i++){
+            if((i-d)<0){
+                index = n-i;
+            }
+            else
+            {
+                
+            }
+        }
+    }
