32a9c87060809f87c83bcde77342a81fa9ef6894
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2022-09-21 01:29:40 +0530

    Day 9 - 21 Sept 2022:2

--- Day9/Triplet_with_zero_sum.cpp
+++ Day9/Triplet_with_zero_sum.cpp
@@ -9,46 +9,52 @@ public:
         // Your code here
         //-3 -1 0 1 2
         //  -2 -1 0 1 3
-        int i = 0;
-        int j = n - 1;
-        int sum = 0;
-        sort(arr, arr + n);
-        while (i < j)
-        {
-            if (arr[i] < 0)
-            {
-                if (abs(arr[i]) > arr[j])
-                {
-                    while (abs(arr[i]) > arr[j])
-                    {
-                        sum += arr[j];
-                        j--;
-
-                        if (sum == 0)
-                        {
-                            return true;
-                        }
+        int i=0;
+        int j = n-1;
+        sort(arr,arr+n);
+        while(i<j){
+            int count = 1;
+            int sum = 0;
+            if(abs(arr[i])>arr[j]){
+                while(abs(arr[i])>arr[j]){
+                    sum+=arr[j];
+                    j--;
+                    count++;
+                    if(sum+arr[i] == 0){
+                        return 1;
                     }
-                }
-                else
-                {
-                    while (abs(arr[i]) < arr[j])
-                    {
-                        sum += arr[i];
+                    if(sum+arr[i] > 0){
                         i++;
-
-                        if (sum == 0)
-                        {
-                            return true;
-                        }
+                        break;
+                    }
+                    if(count>3){
+                        i++;
+                        break;
                     }
                 }
             }
             else
             {
-                return 0;
+                while(arr[j]>abs(arr[i])){
+                    sum+=arr[i];
+                    i++;
+                    if(sum+arr[j] == 0){
+                        return 1;
+                    }
+                    if(sum+arr[j]<0){
+                        j--;
+                        break;
+                    }
+                    if(count>3){
+                        j--;
+                        break;
+                    }
+                }
             }
         }
+        
+        return 0;
+    }
     }
 };
 int main()
