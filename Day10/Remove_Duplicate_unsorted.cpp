1828312f9de15c8a131f56e94fceff302b15912e
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2022-09-22 00:38:34 +0530

    Day 10 - 22 Sept 2022:1

--- Day10/Remove_Duplicate_unsorted.cpp
+++ Day10/Remove_Duplicate_unsorted.cpp
@@ -2,60 +2,61 @@
 using namespace std;
 class Solution
 {
-public:
-    Node *deletenode(Node *&head)
-    {
-        // position 1
-        if (position == 1)
-        {
-            Node *temp = head;
-            head = head->next;
-            temp->next = NULL;
-            delete temp;
-        }
-        Node *curr = head;
-        Node *prev = NULL;
-        int count = 1;
-
-        while (count < position)
-        {
-            prev = curr;
-            curr = curr->next;
-        }
-
-        prev->next = curr->next;
-        curr->next = NULL;
-
-        delete curr;
+    public:
+    int getlength(Node *head, int length){
+        
+    if(head->next == NULL) return length;
+    return getlength(head->next,length+1);
+    
     }
 
-    // Function to remove duplicates from unsorted linked list.
-    Node *removeDuplicates(Node *head)
+    void deletenode(Node* &head , int position){
+        //position 1
+        if(position==1){
+            Node* temp = head;
+            // head = head -> next;
+            // temp -> next = NULL;
+            delete temp;
+        }
+        Node* curr = head;
+        Node* prev = NULL;
+        int count = 1;
+        
+        while(count<position){
+            prev = curr;
+            curr = curr -> next;
+        }
+        
+        prev-> next = curr -> next;
+        curr -> next = NULL;
+        
+        delete curr;
+    }
+    
+    //Function to remove duplicates from unsorted linked list.
+    Node * removeDuplicates(Node *head) 
     {
-        // your code goes here
-        unordered_map<int, int> m;
-        Node *temp = head;
-        while (temp != NULL)
-        {
-            m[temp->data]++;
-            temp = temp->next;
-        }
-
-        Node *temp2 = head;
-        while (temp2 != NULL)
-        {
-            bool flag = false;
-            Node *now = temp2;
-            if (m[temp->data] > 2)
-            {
-                flag = true;
+     // your code goes here
+     map<int , bool> check;
+        Node* prev = NULL;
+        Node* temp = head;
+        
+        while(temp != NULL){
+            if(check[temp->data] != true){
+                check[temp->data] = true;
+                prev = temp;
+                temp = temp->next;
             }
-            if (m[temp->data] > 1 && flag == true)
-            {
-                delete now;
-                m[temp->data] = 1;
+            else{
+                prev->next = temp->next;
+                Node* del = temp;
+                temp = temp->next;
+                
+                delete(del);
             }
         }
+        
+        return head;
     }
 };
 int main()
