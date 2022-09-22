eac905a8a9a1327a8690ddc618f327c936a4f77f
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2022-09-22 10:25:20 +0530

    Day 11 - 22 Sept 2022:1

---
+++ Day11/Reverse_a_Doubly_LL.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+Node* reverseDLL(Node * head)
+{
+    //Your code here
+    if(head == NULL || head-> next == NULL)
+    {
+        // head = NULL;
+        return head;
+    }
+
+    Node* prev1 = NULL;
+    Node* curr = head;
+    Node* forward = NULL;
+    // tail = curr;
+
+    while(curr!=NULL)
+    {
+        forward = curr->next;
+        if(curr->next!=NULL)
+            forward -> prev = curr;
+        curr -> next = prev1;
+        curr -> prev = forward;
+        prev1 = curr;
+        curr = forward;
+    }
+
+    head = prev1;
+    
+    return head;
+    
+}
+int main()
+{
+    
+    return 0;
+}
