8e91c96a0901ac00a815c36f44d6fc98b3dd8032
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2022-11-13 01:25:28 +0530

    Day 19 - 12 Nov 2022:2

--- Day19/valid_parenthesis.cpp
+++ Day19/valid_parenthesis.cpp
@@ -6,26 +6,31 @@ public:
         stack<char> st;
         for(int i=0;i<s.length();i++){
             // int flag = -1;
-            if(st.empty()){
-                // flag = 1;
-                st.push(s[i]);
+            // if(st.empty()){
+            //     flag = 1;
+            //     st.push(s[i]);
+            // }
+            if(s.length() == 0){
+                return true;
             }
             // char top = st.peek();
-            char top = st.top();
+            // char top = st.top();
             if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
                 // if(flag==0)
                 st.push(s[i]);
-                char top = st.top();
-                flag = 0;
+                // char top = st.top();
+                 //flag = 0;
             }
-            else if((s[i] == ')' && top == '(') || (s[i] == ']' && top == '[') || (s[i] == '}' && top == '{')){
+            else if((s[i] == ')' && st.empty() == 0 && st.top() == '(') || (s[i] == ']' && st.empty() == 0 && st.top() == '[') || (s[i] == '}' && st.empty() == 0 && st.top() == '{')){
                 st.pop();
             }
             else{
                 return false;
             }        
         }
-        return true;
+        if(st.empty() == 1)
+            return true;
+        return false;
     }
 };
 int main()
