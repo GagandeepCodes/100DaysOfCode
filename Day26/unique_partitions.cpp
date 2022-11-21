3f4fc51bf70e9b8be08781c8b9b2fed56304cd28
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2022-11-22 00:33:34 +0530

    Day 26 - 22 Nov 2022:1

---
+++ Day26/unique_partitions.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>> UniquePartitions(int n)
+{
+
+    // Code here
+
+    vector<vector<int>> ans;
+
+    vector<int> list;
+
+    f(n, ans, list);
+
+    return ans;
+}
+
+void f(int n, vector<vector<int>> &ans, vector<int> &list)
+{
+
+    if (n <= 0)
+    {
+
+        if (n == 0)
+        {
+
+            vector<int> list1;
+
+            list1.assign(list.begin(), list.end());
+
+            ans.push_back(list1);
+        }
+
+        return;
+    }
+
+    int v = n;
+
+    if (list.size() > 0)
+        v = list[list.size() - 1];
+
+    for (int i = v; i >= 1; i--)
+    {
+
+        list.push_back(i);
+
+        f(n - i, ans, list);
+
+        list.pop_back();
+    }
+}
+int main()
+{
+
+    return 0;
+}
