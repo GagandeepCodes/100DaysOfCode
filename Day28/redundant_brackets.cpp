b19585eea8861d871f0f20475209f637af1f9f89
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2022-12-20 01:23:58 +0530

    Day 28 - 20 Dec 2022:1

---
+++ Day28/redundant_brackets.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int checkRedundancy(string s) {
+        // code here
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            char ch = s[i];
+            if(ch =='(' || ch == '+' || ch == '-' || ch =='*' || ch =='/'){
+                st.push(ch);
+            }
+            else
+            {
+                if(ch == ')'){
+                    int flag = 1;
+                    while(st.top()!='('){
+                        if(ch == '+' || ch == '-' || ch =='*' || ch =='/'){
+                            flag = 0;
+                        }
+                        st.pop();
+                    }
+                    if(flag==1){
+                        return 1;
+                    }
+                    st.pop();
+                }
+            }
+        }
+        return 0;
+    }
+int main()
+{
+    
+    return 0;
+}
