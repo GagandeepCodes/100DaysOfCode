a4cd1a51c746ad297c2c1f3087c3c458c30fa372
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2022-12-21 22:08:25 +0530

    Largest area of histogram STACK

---
+++ Day30/largest_area_histogram.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+public:
+// Function to find largest rectangular area possible in a given histogram.
+vector<int> nextSmall(long long arr[], int n)
+
+{
+
+    vector<int> ans;
+
+    stack<int> stk;
+
+    for (int i = n - 1; i >= 0; i--)
+
+    {
+
+        while (!stk.empty() and arr[stk.top()] >= arr[i])
+
+            stk.pop();
+
+        int x = !stk.empty() ? stk.top() : n;
+
+        stk.push(i);
+
+        ans.push_back(x);
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    return ans;
+}
+
+vector<int> prevSmall(long long arr[], int n)
+
+{
+
+    vector<int> ans;
+
+    stack<int> stk;
+
+    for (int i = 0; i < n; i++)
+
+    {
+
+        while (!stk.empty() and arr[stk.top()] >= arr[i])
+
+        {
+
+            stk.pop();
+        }
+
+        int x = stk.empty() ? -1 : stk.top();
+
+        stk.push(i);
+
+        ans.push_back(x);
+    }
+
+    return ans;
+}
+
+long long getMaxArea(long long arr[], int n)
+
+{
+
+    // Your code here
+
+    vector<int> ns = nextSmall(arr, n);
+
+    vector<int> ps = prevSmall(arr, n);
+
+    long long ans = 0;
+
+    for (int i = 0; i < n; i++)
+
+    {
+
+        long long res = arr[i] * (ns[i] - ps[i] - 1);
+
+        ans = max(res, ans);
+    }
+
+    return ans;
+}
+int main()
+{
+
+    return 0;
+}
