6cddb86ddaba1d7bc1e45e1b914294b9613f7c57
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2022-12-29 19:43:40 +0530

    Day38 - 29 Dec 2022:1

---
+++ Day38/POTD_gfg.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> asteroidCollision(int N, vector<int> &asteroids)
+{
+    stack<int> st;
+    for (int i = 0; i < asteroids.size(); i++)
+    {
+        if (asteroids[i] > 0)
+        {
+            st.push(asteroids[i]);
+        }
+        else
+        {
+            while (!st.empty())
+            {
+                if (st.top() < 0)
+                {
+                    st.push(asteroids[i]);
+                    break;
+                }
+                else if (st.top() < -1 * asteroids[i])
+                {
+                    st.pop();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (!st.empty() && st.top() == -1 * asteroids[i])
+            {
+                st.pop();
+            }
+
+            if (st.empty())
+            {
+                st.push(asteroids[i]);
+            }
+        }
+    }
+    vector<int> res;
+    while (!st.empty())
+    {
+        res.push_back(st.top());
+        st.pop();
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+int main()
+{
+
+    return 0;
+}
